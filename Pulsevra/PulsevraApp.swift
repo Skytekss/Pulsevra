01afdba001bbbe5c7a114a25be4469d5b31a7eaa
Ilia Grinko <grink2000@mail.ru>
2025-08-19 14:23:01 +0300

    Initial commit

---
+++ Pulsevra/PulsevraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsevraApp.swift
+//  Pulsevra
+//
+//  Created by Ilia Grinko on 19.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PulsevraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
